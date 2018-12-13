f7193fb5dba2f87a3454f82bb6dd407266c81afd
KhunNooDev <noo_okok@hotmail.com>
2018-12-13 23:10:57 +0700

    Updates

---
+++ precache-manifest.c92375ec6becf72746e56722a019b784.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/www/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "261c5d8f758ea0df9755",
+    "url": "/www/static/css/main.de336714.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/www/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "0212dcee4f1ffafb6fe5",
+    "url": "/www/static/js/1.0212dcee.chunk.js"
+  },
+  {
+    "revision": "ab720888b7f38f2d6407",
+    "url": "/www/static/js/runtime~main.ab720888.js"
+  },
+  {
+    "revision": "261c5d8f758ea0df9755",
+    "url": "/www/static/js/main.261c5d8f.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/www/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/www/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/www/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "0212dcee4f1ffafb6fe5",
+    "url": "/www/static/css/1.7967b3cc.chunk.css"
+  },
+  {
+    "revision": "82378877de8863120c65f27f009b369f",
+    "url": "/www/index.html"
+  }
+];
